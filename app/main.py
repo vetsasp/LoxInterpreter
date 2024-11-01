378384f7214e40920848f33c8f252b59f6c66805
Philip Vetsas <vetsasp@gmail.com>
2024-11-01 17:47:17 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -27,6 +27,20 @@ def main():
             print("LEFT_BRACE { null")
         elif c == "}":
             print("RIGHT_BRACE } null")
+        elif c == ",":
+            print("COMMA , null")
+        elif c == ".":
+            print("DOT . null")
+        elif c == "-":
+            print("MINUS - null")
+        elif c == "+":
+            print("PLUS + null")
+        elif c == ";":
+            print("SEMICOLON ; null")
+        elif c == "*":
+            print("STAR * null")
+        else:
+            print(f"ILLEGAL {c} null")
 
     
     print("EOF  null") # Placeholder, remove this line when implementing the scanner
