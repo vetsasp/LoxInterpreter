b8782a244ec8fdcc54da2017bf45e629230ecd48
Philip Vetsas <vetsasp@gmail.com>
2024-11-01 17:09:48 -0400

    uncommenting given code

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@ import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
+    # print("Logs from your program will appear here!", file=sys.stderr)
 
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
@@ -19,11 +18,10 @@ def main():
     with open(filename) as file:
         file_contents = file.read()
 
-    # Uncomment this block to pass the first stage
-    # if file_contents:
-    #     raise NotImplementedError("Scanner not implemented")
-    # else:
-    #     print("EOF  null") # Placeholder, remove this line when implementing the scanner
+    if file_contents:
+        raise NotImplementedError("Scanner not implemented")
+    else:
+        print("EOF  null") # Placeholder, remove this line when implementing the scanner
 
 
 if __name__ == "__main__":
